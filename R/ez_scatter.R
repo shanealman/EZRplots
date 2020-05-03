dfae1a8d2149e18e4ff68a42a072a94b83834d88
Ari Arango <asarango@calpoly.edu>
2020-05-03 14:54:21 -0700

    added ez_labels

--- R/ez_scatter.R
+++ R/ez_scatter.R
@@ -53,3 +53,31 @@ ez_scatter <- function(data, x_num, y_num, z_cat = NULL, color = NULL, shape = N
   }
 }
 
+#' Add graph titles, axis labels, and customize label colors and typography for plots
+#'
+#' @param plot name of plot you would like to add labels to. (required)
+#' @param title title of the plot, input as string (required)
+#' @param x x-axis label, input as string (optional)
+#' @param y y-axis label, input as string (optional)
+#' @param title_color color of title, input as string (optional)
+#' @param axis_color color for axis labels, input as string (optional)
+#' @param title_type typography for title, input as string (optional)
+#' @param axis_type typography for axis labels, input as string (optional)
+#'
+#' @return A plot
+#'
+#' @import ggplot2
+#'
+#' @export
+#'
+
+ez_labels <- function(plot, title, x = NULL, y = NULL, title_color = NULL, axis_color = NULL, title_type = NULL, axis_type = NULL){
+
+  plot +
+    labs( title = title, x = x, y = y) +
+    theme(
+      plot.title = element_text(color = title_color, size = 20, face = title_type),
+      axis.title.x = element_text(color = axis_color, size = 14, face = axis_type),
+      axis.title.y = element_text(color = axis_color, size = 14, face = axis_type)
+    )
+}
