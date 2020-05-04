4dfac79c2e15c0b3a7ada0abecb779395b7af865
Ari Arango <asarango@calpoly.edu>
2020-05-03 18:28:05 -0700

    assert?

--- tests/testthat/test-test-ez_scatter.R
+++ tests/testthat/test-test-ez_scatter.R
@@ -5,21 +5,21 @@ test_that("ez_scatter function works", {
 
   my_plot1 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num = "Sepal.Length"))
 
-  assert_equal(my_plot1, correct_plot1)
+  assert(my_plot1, correct_plot1)
 
   correct_plot2 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(shape = 3))
 
   my_plot2 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = 3))
 
-  assert_equal(my_plot2, correct_plot2)
+  assert(my_plot2, correct_plot2)
 
   correct_plot3 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(color = "steelblue", shape = 3))
 
   my_plot3 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", color = "steelblue", shape = 3))
 
-  assert_equal(my_plot3, correct_plot3)
+  assert(my_plot3, correct_plot3)
 
   correct_plot4 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(aes_string(color = "Species")) +
@@ -27,14 +27,14 @@ test_that("ez_scatter function works", {
 
   my_plot4 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2"))
 
-  assert_equal(my_plot4, correct_plot4)
+  assert(my_plot4, correct_plot4)
 
   correct_plot5 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(aes_string(shape = shape)))
 
   my_plot5 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = "Species"))
 
-  assert_equal(my_plot5, correct_plot5)
+  assert(my_plot5, correct_plot5)
 
   correct_plot6 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(aes_string(color = "Species", shape = "Species")) +
@@ -42,6 +42,6 @@ test_that("ez_scatter function works", {
 
   my_plot6 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species"))
 
-  assert_equal(my_plot6, correct_plot6)
+  assert(my_plot6, correct_plot6)
 
 })
