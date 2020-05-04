3c2add6e59a05d26c627d0dc41645e8a62f0ecaa
Ari Arango <asarango@calpoly.edu>
2020-05-03 17:22:03 -0700

    starting tests

--- tests/testthat/test-test-ez_scatter.R
+++ tests/testthat/test-test-ez_scatter.R
@@ -1,3 +1,39 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("ez_scatter function works", {
+
+  correct_result1 <- ggplot(data, aes_string(x = x_num, y = y_num)) +
+    geom_point()
+
+  my_plot1 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  correct_result <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(aes_string(color = "Species", shape = "Species")) +
+    scale_colour_brewer(palette = "Dark2")
+
+  my_plot <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species")
+
+  expect_equal(my_result, correct_result)
+
 })
+
+
+
+
+
+#data, x_num, y_num, z_cat = NULL, color = NULL, shape = NULL
