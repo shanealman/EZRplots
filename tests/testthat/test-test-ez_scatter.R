33cd94b4a728935540ff70ae3831f3261fdc9cc5
Ari Arango <asarango@calpoly.edu>
2020-05-03 17:53:24 -0700

    Adding all my tests for ez_scatter

--- tests/testthat/test-test-ez_scatter.R
+++ tests/testthat/test-test-ez_scatter.R
@@ -1,39 +1,47 @@
 test_that("ez_scatter function works", {
 
-  correct_result1 <- ggplot(data, aes_string(x = x_num, y = y_num)) +
+  correct_plot1 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point()
 
-  my_plot1 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species")
+  my_plot1 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length")
 
+  expect_equal(my_plot1, correct_plot1)
 
+  correct_plot2 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(shape = 23)
 
+  my_plot2 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = 23)
 
+  expect_equal(my_plot2, correct_plot2)
 
+  correct_plot3 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(color = "steelblue", shape = 23)
 
+  my_plot3 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", color = "steelblue", shape = 23)
 
+  expect_equal(my_plot3, correct_plot3)
 
+  correct_plot4 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(aes_string(color = "Species")) +
+    scale_colour_brewer(palette = "Dark2")
 
+  my_plot4 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2")
 
+  expect_equal(my_plot4, correct_plot4)
 
+  correct_plot5 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(aes_string(shape = shape))
 
+  my_plot5 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = "Species")
 
+  expect_equal(my_plot5, correct_plot5)
 
-
-
-
-
-  correct_result <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+  correct_plot5 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(aes_string(color = "Species", shape = "Species")) +
     scale_colour_brewer(palette = "Dark2")
 
-  my_plot <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species")
+  my_plot5 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species")
 
-  expect_equal(my_result, correct_result)
+  expect_equal(my_plot5, correct_plot5)
 
 })
-
-
-
-
-
-#data, x_num, y_num, z_cat = NULL, color = NULL, shape = NULL
