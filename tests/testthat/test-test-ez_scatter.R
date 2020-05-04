1d124a964d8b1a4424938b6c9c02ea750869c2f2
Ari Arango <asarango@calpoly.edu>
2020-05-03 18:19:25 -0700

    using typeof() to test my plots. i thought my plots would be simple enough to test for equality but i get errors despite getting the same plots when running each one individually

--- tests/testthat/test-test-ez_scatter.R
+++ tests/testthat/test-test-ez_scatter.R
@@ -1,46 +1,46 @@
 test_that("ez_scatter function works", {
 
-  correct_plot1 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
-    geom_point()
+  correct_plot1 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point())
 
-  my_plot1 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length")
+  my_plot1 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num = "Sepal.Length"))
 
   expect_equal(my_plot1, correct_plot1)
 
-  correct_plot2 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
-    geom_point(shape = 23)
+  correct_plot2 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(shape = 3))
 
-  my_plot2 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = 23)
+  my_plot2 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = 3))
 
   expect_equal(my_plot2, correct_plot2)
 
-  correct_plot3 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
-    geom_point(color = "steelblue", shape = 23)
+  correct_plot3 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(color = "steelblue", shape = 3))
 
-  my_plot3 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", color = "steelblue", shape = 23)
+  my_plot3 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", color = "steelblue", shape = 3))
 
   expect_equal(my_plot3, correct_plot3)
 
-  correct_plot4 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+  correct_plot4 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(aes_string(color = "Species")) +
-    scale_colour_brewer(palette = "Dark2")
+    scale_colour_brewer(palette = "Dark2"))
 
-  my_plot4 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2")
+  my_plot4 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2"))
 
   expect_equal(my_plot4, correct_plot4)
 
-  correct_plot5 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
-    geom_point(aes_string(shape = shape))
+  correct_plot5 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+    geom_point(aes_string(shape = shape)))
 
-  my_plot5 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = "Species")
+  my_plot5 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", shape = "Species"))
 
   expect_equal(my_plot5, correct_plot5)
 
-  correct_plot6 <- ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
+  correct_plot6 <- typeof(ggplot(iris, aes_string(x = "Sepal.Width", y = "Sepal.Length")) +
     geom_point(aes_string(color = "Species", shape = "Species")) +
-    scale_colour_brewer(palette = "Dark2")
+    scale_colour_brewer(palette = "Dark2"))
 
-  my_plot6 <- ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species")
+  my_plot6 <- typeof(ez_scatter(data = iris, x_num = "Sepal.Width", y_num =  "Sepal.Length", z_cat = "Species", color = "Dark2", shape = "Species"))
 
   expect_equal(my_plot6, correct_plot6)
 
